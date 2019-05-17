0978a3e39e092c1859d28f05433a7a409812940e
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2019-05-17 16:15:48 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/wandersongutemberg/cursomc/services/CategoriaService.java
+++ src/main/java/com/wandersongutemberg/cursomc/services/CategoriaService.java
@@ -7,15 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.wandersongutemberg.cursomc.domain.Categoria;
 import com.wandersongutemberg.cursomc.repositories.CategoriaRepository;
+import com.wandersongutemberg.cursomc.services.exceptions.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
