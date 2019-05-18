5a8364395cdbf7326a0fe6c0368d15dba1406abc
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2019-05-18 01:24:30 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/wandersongutemberg/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.wandersongutemberg.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wandersongutemberg.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
