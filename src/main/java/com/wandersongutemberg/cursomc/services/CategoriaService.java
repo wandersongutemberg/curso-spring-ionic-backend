c80668febc4d0a580578ccb246ef4fcf4a9cba70
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2019-05-20 20:48:12 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/wandersongutemberg/cursomc/services/CategoriaService.java
+++ src/main/java/com/wandersongutemberg/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wandersongutemberg.cursomc.domain.Categoria;
+import com.wandersongutemberg.cursomc.dto.CategoriaDTO;
 import com.wandersongutemberg.cursomc.repositories.CategoriaRepository;
 import com.wandersongutemberg.cursomc.services.exceptions.DataIntegrityException;
 import com.wandersongutemberg.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 	{
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
