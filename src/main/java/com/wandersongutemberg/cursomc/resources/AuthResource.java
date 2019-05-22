f2c28c371599241a1c15f072dbe1196e5116a7c3
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2019-05-22 20:00:53 -0300

    Expondo o header authorization

--- src/main/java/com/wandersongutemberg/cursomc/resources/AuthResource.java
+++ src/main/java/com/wandersongutemberg/cursomc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.wandersongutemberg.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wandersongutemberg.cursomc.dto.EmailDTO;
 import com.wandersongutemberg.cursomc.security.JWTUtil;
 import com.wandersongutemberg.cursomc.security.UserSS;
+import com.wandersongutemberg.cursomc.services.AuthService;
 import com.wandersongutemberg.cursomc.services.UserService;
 
 @RestController
@@ -18,20 +22,22 @@ public class AuthResource {
 	
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers","Authorization");
 		return ResponseEntity.noContent().build();
 	}
 	
 	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
-	public ResponseEntity<Void> forgot(HttpServletResponse response) {
-		UserSS user = UserService.authenticated();
-		String token = jwtUtil.generateToken(user.getUsername());
-		response.addHeader("Authorization", "Bearer " + token);
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
