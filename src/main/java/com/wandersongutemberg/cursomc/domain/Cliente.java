7d503bd96c1ac0a2cb98904c65dc245f24f58e4f
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2019-05-29 22:20:23 -0300

    validacao customizada: email nao repetido na insercao de cliente

--- src/main/java/com/wandersongutemberg/cursomc/domain/Cliente.java
+++ src/main/java/com/wandersongutemberg/cursomc/domain/Cliente.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -29,6 +30,8 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@Column(unique=true)
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
