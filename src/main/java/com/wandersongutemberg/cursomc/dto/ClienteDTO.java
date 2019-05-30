f8c2d322d8ed19bc91550570d2060816df04c118
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2019-05-29 22:12:31 -0300

    validacao customizada de CPF ou CNPJ na insercao de cliente

--- src/main/java/com/wandersongutemberg/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/wandersongutemberg/cursomc/dto/ClienteDTO.java
@@ -13,6 +13,7 @@ public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
 	@NotEmpty(message="Preenchimento obrigatório")
 	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
