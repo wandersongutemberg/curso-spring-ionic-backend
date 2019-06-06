c91a50ce990159f56c8de4173a4ea847fb3457b9
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2019-06-06 01:16:51 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/wandersongutemberg/cursomc/services/DBService.java
+++ src/main/java/com/wandersongutemberg/cursomc/services/DBService.java
@@ -114,7 +114,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "nelio.cursos@gmail.com", "36378912377",
+		Cliente cli1 = new Cliente(null, "Maria Silva", "wanderson.gutembergs@gmail.com", "36378912377",
 				TipoCliente.PESSOAFISICA, pe.encode("123"));
 
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
